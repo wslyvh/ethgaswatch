2394acc48895933b00f6bfd9e4cff19120940c5d
wslyvh <wslyvh@users.noreply.github.com>
2020-08-12 17:32:42 +0200

    cra

---
+++ web/src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './assets/index.css';
+import App from './pages/app';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
---
+++ web/src/pages/app.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>ETH Gas Tracker</h1>
+      </header>
+      <div>
+        <h2>Recommended Gas Prices</h2>
+        <div>
+          9.000
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
