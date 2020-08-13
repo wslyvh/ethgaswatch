e5131ff38c06ada2abb2101a0acbd09cdbedb0f0
wslyvh <wslyvh@users.noreply.github.com>
2020-08-13 16:50:21 +0200

    functions

--- functions/hello.js
+++ functions/hello.js
@@ -1,3 +1,3 @@
 exports.handler = function(event, context, callback) {
-    callback(null, { statusCode: 200, body: "Hello World" });
+    callback(null, { statusCode: 200, body: "Hello Root" });
 }
