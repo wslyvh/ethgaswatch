50fb47174041029e4c47f89fd4050abff42a3cb6
wslyvh <wslyvh@users.noreply.github.com>
2020-08-18 18:31:31 +0200

    gasnow

--- web/functions/gasnow.js
+++ web/functions/gasnow.js
@@ -36,6 +36,7 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
     }
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+var fetch = require("node-fetch");
 function handler(event, context) {
     return __awaiter(this, void 0, void 0, function () {
         var response, body;
