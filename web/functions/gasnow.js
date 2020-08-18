48df428f5ff28694894c15743fa9ce28f7c2f987
wslyvh <wslyvh@users.noreply.github.com>
2020-08-18 14:42:37 +0200

    async

--- web/functions/gasnow.js
+++ web/functions/gasnow.js
@@ -1,4 +1,4 @@
-exports.handler = function(event, context, callback) {
+exports.handler = async function(event, context, callback) {
 
     const response = await fetch(`https://www.gasnow.org/api/v1/gas/price`);
     console.log(response);
