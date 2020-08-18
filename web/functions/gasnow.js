fe2866c360b83cbe37528f6e55e9f4f6a3817f99
wslyvh <wslyvh@users.noreply.github.com>
2020-08-18 10:55:14 +0200

    gasnow function

---
+++ web/functions/gasnow.js
@@ -0,0 +1,8 @@
+exports.handler = function(event, context, callback) {
+
+    const response = await fetch(`https://www.gasnow.org/api/v1/gas/price`);
+    console.log(response);
+    const body = await response.json();
+
+    callback(null, { statusCode: 200, body: body });
+}
