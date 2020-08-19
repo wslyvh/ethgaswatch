97db91db97d349f6b56216d2508063a4ebdb6507
wslyvh <wslyvh@users.noreply.github.com>
2020-08-19 11:31:43 +0200

    gasnow

--- web/src/functions/gasnow.ts
+++ web/src/functions/gasnow.ts
@@ -6,6 +6,7 @@ export async function handler(event: APIGatewayEvent, context: Context) {
     
     const res = await fetch(`https://www.gasnow.org/api/v1/gas/price`);
     const body = await res.json();
+    console.log(body);
 
-    return { statusCode: 200, body: body }
+    return { statusCode: 200, body: JSON.stringify(body) }
 }
