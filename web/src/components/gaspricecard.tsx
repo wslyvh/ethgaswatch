99a49c344c9dd4677be9802f936f930d74a5e290
wslyvh <wslyvh@users.noreply.github.com>
2020-08-20 13:51:53 +0200

    wait times

--- web/src/components/gaspricecard.tsx
+++ web/src/components/gaspricecard.tsx
@@ -7,10 +7,25 @@ interface GasPriceProps {
 }
 
 export const GasPriceCard = (props: GasPriceProps) => {
+
+    let waitTime = 0;
+    if (props.title === "Fast") { 
+        waitTime = 2;
+    }
+    if (props.title === "Normal") { 
+        waitTime = 5;
+    }
+    if (props.title === "Slow") { 
+        waitTime = 30;
+    }
+
     return (
         <div className="card shadow-sm rounded">
             <h3 className="card-title">{props.title}</h3>
-            <p className="card-text">{props.price} gwei</p>
+            <p className="card-text">
+                {props.price} gwei <br/>
+                <small className="text-muted">&lt;{waitTime} min</small>
+            </p>            
         </div>
     )
 }
