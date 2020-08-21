22a19ea7c6ea48dcb64a83e3eab7ec829060d5b1
wslyvh <wslyvh@users.noreply.github.com>
2020-08-21 10:26:22 +0200

    hello

--- web/src/functions/hello.ts
+++ web/src/functions/hello.ts
@@ -1,6 +1,9 @@
 import { Context, APIGatewayEvent } from 'aws-lambda'
+import { AppConfig } from '../config/app'
 
 export async function handler(event: APIGatewayEvent, context: Context) {
+    console.log("Hello", AppConfig.NODE_ENV);
+
     return {
         statusCode: 200,
         body: `Hello World!`
