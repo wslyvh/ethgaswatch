fee7c50fa644e7ca250a14650fdbfedbb47e8946
wslyvh <wslyvh@users.noreply.github.com>
2020-09-02 10:01:41 +0200

    promises

--- web/src/services/GasService.ts
+++ web/src/services/GasService.ts
@@ -6,50 +6,19 @@ import { AVERAGE_NAME } from '../utils/constants';
 require('encoding');
 
 export async function GetAllPrices(includeAverage?: boolean): Promise<RecommendedGasPrices[]> { 
-
+    
     const results = new Array<RecommendedGasPrices>();
-    try {
-        const prices = await fromGasStation();
-        results.push(prices);
-    } catch (ex) { 
-        console.log("Couldn't retrieve data from GAS STATION", ex);
-    }
-    try {
-        const prices = await fromEtherscan();
-        results.push(prices);
-    } catch (ex) { 
-        console.log("Couldn't retrieve data from ETHERSCAN", ex);
-    }
-    try {
-        const prices = await fromEtherchain();
-        results.push(prices);
-    } catch (ex) { 
-        console.log("Couldn't retrieve data from ETHERCHAIN", ex);
-    }    
-    try {
-        const prices = await fromGasNow();
-        results.push(prices);
-    } catch (ex) { 
-        console.log("Couldn't retrieve data from GASNOW", ex);
-    }
-    try {
-        const prices = await fromMyCrypto();
-        results.push(prices);
-    } catch (ex) { 
-        console.log("Couldn't retrieve data from MyCrypto", ex);
-    }
-    try {
-        const prices = await fromPoaNetwork();
-        results.push(prices);
-    } catch (ex) { 
-        console.log("Couldn't retrieve data from POA Network", ex);
-    }
-    try {
-        const prices = await fromUpvest();
-        results.push(prices);
-    } catch (ex) { 
-        console.log("Couldn't retrieve data from UPVEST", ex);
-    }
+
+    await Promise.all([
+        await fromEtherscan(),
+        await fromEtherchain(),
+        await fromGasStation(),
+        await fromGasNow(),
+        await fromMyCrypto(),
+        await fromPoaNetwork(),
+        await fromUpvest()].map(i => {
+        if(i) results.push(i);
+    }));
 
     if (includeAverage) {
         const medianPrices = Average(results, true);
@@ -67,111 +36,152 @@ export async function GetAveragePrice(): Promise<RecommendedGasPrices> {
     return Average(results, true);
 }
 
-export async function fromEtherscan(): Promise<RecommendedGasPrices> { 
+export async function fromEtherscan(): Promise<RecommendedGasPrices | null> { 
 
-    const response = await fetch(`https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${AppConfig.ETHERSCAN_APIKEY}`);
-    const body = await response.json();
+    try { 
+        const response = await fetch(`https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${AppConfig.ETHERSCAN_APIKEY}`);
+        const body = await response.json();
 
-    return {
-        name: "Etherscan",
-        source: "https://etherscan.io/gastracker",
-        fast: Math.round(Number(body.result.FastGasPrice)),
-        average: Math.round(Number(body.result.ProposeGasPrice)),
-        low: Math.round(Number(body.result.SafeGasPrice)),
-        lastBlock: Number(body.result.LastBlock)
-    } as RecommendedGasPrices;
+        return {
+            name: "Etherscan",
+            source: "https://etherscan.io/gastracker",
+            fast: Math.round(Number(body.result.FastGasPrice)),
+            average: Math.round(Number(body.result.ProposeGasPrice)),
+            low: Math.round(Number(body.result.SafeGasPrice)),
+            lastBlock: Number(body.result.LastBlock)
+        } as RecommendedGasPrices;
+
+    } catch (ex) { 
+        console.log("Couldn't retrieve data from ETHERSCAN", ex);
+        return null;
+    }
 }
 
-export async function fromEtherchain(): Promise<RecommendedGasPrices> { 
+export async function fromEtherchain(): Promise<RecommendedGasPrices | null> { 
 
-    const response = await fetch(`https://www.etherchain.org/api/gasPriceOracle`);
-    const body = await response.json();
+    try { 
+        const response = await fetch(`https://www.etherchain.org/api/gasPriceOracle`);
+        const body = await response.json();
 
-    return {
-        name: "Etherchain",
-        source: "https://etherchain.org/tools/gasPriceOracle",
-        fast: Number(body.fast),
-        average: Number(body.standard),
-        low: Number(body.safeLow),
-        lastUpdate: Date.now()
-    } as RecommendedGasPrices;
+        return {
+            name: "Etherchain",
+            source: "https://etherchain.org/tools/gasPriceOracle",
+            fast: Number(body.fast),
+            average: Number(body.standard),
+            low: Number(body.safeLow),
+            lastUpdate: Date.now()
+        } as RecommendedGasPrices;
+
+    } catch (ex) { 
+        console.log("Couldn't retrieve data from ETHERCHAIN", ex);
+        return null;
+    }
 }
 
-export async function fromGasStation(): Promise<RecommendedGasPrices> { 
+export async function fromGasStation(): Promise<RecommendedGasPrices | null> { 
 
-    const response = await fetch(`https://ethgasstation.info/api/ethgasAPI.json?api-key=${AppConfig.GASSTATION_APIKEY}`);
-    const body = await response.json();
+    try { 
+        const response = await fetch(`https://ethgasstation.info/api/ethgasAPI.json?api-key=${AppConfig.GASSTATION_APIKEY}`);
+        const body = await response.json();
 
-    return {
-        name: "Gas station",
-        source: "https://ethgasstation.info/",
-        fast: Math.round(body.fastest / 10),
-        average: Math.round(body.average / 10),
-        low: Math.round(body.safeLow / 10),
-        lastBlock: Number(body.blockNum)
-    } as RecommendedGasPrices;
+        return {
+            name: "Gas station",
+            source: "https://ethgasstation.info/",
+            fast: Math.round(body.fastest / 10),
+            average: Math.round(body.average / 10),
+            low: Math.round(body.safeLow / 10),
+            lastBlock: Number(body.blockNum)
+        } as RecommendedGasPrices;
+
+    } catch (ex) { 
+        console.log("Couldn't retrieve data from GAS STATION", ex);
+        return null;
+    }
 }
 
-export async function fromGasNow(): Promise<RecommendedGasPrices> { 
+export async function fromGasNow(): Promise<RecommendedGasPrices | null> { 
 
-    const response = await fetch(`https://www.gasnow.org/api/v1/gas/price`);
-    const body = await response.json();
+    try { 
+        const response = await fetch(`https://www.gasnow.org/api/v1/gas/price`);
+        const body = await response.json();
 
-    return {
-        name: "GAS Now",
-        source: "https://www.gasnow.org/",
-        fast: Math.round(WeiToGwei(body.data.top50)),
-        average: Math.round(WeiToGwei(body.data.top200)),
-        low: Math.round(WeiToGwei(body.data.top400)),
-        lastUpdate: Number(body.data.timestamp)
-    } as RecommendedGasPrices;
+        return {
+            name: "GAS Now",
+            source: "https://www.gasnow.org/",
+            fast: Math.round(WeiToGwei(body.data.top50)),
+            average: Math.round(WeiToGwei(body.data.top200)),
+            low: Math.round(WeiToGwei(body.data.top400)),
+            lastUpdate: Number(body.data.timestamp)
+        } as RecommendedGasPrices;
+
+    } catch (ex) { 
+        console.log("Couldn't retrieve data from GASNOW", ex);
+        return null;
+    }
 }
 
-export async function fromMyCrypto(): Promise<RecommendedGasPrices> { 
+export async function fromMyCrypto(): Promise<RecommendedGasPrices | null> { 
     
-    const response = await fetch(`https://gas.mycryptoapi.com/`);
-    const body = await response.json();
+    try { 
+        const response = await fetch(`https://gas.mycryptoapi.com/`);
+        const body = await response.json();
 
-    return {
-        name: "MyCrypto",
-        source: "https://gas.mycryptoapi.com/",
-        fast: Math.round(body.fastest),
-        average: Math.round(body.standard),
-        low: Math.round(body.safeLow),
-        lastBlock: Number(body.blockNum)
-    } as RecommendedGasPrices;
+        return {
+            name: "MyCrypto",
+            source: "https://gas.mycryptoapi.com/",
+            fast: Math.round(body.fastest),
+            average: Math.round(body.standard),
+            low: Math.round(body.safeLow),
+            lastBlock: Number(body.blockNum)
+        } as RecommendedGasPrices;
+
+    } catch (ex) { 
+        console.log("Couldn't retrieve data from MYCRYPTO", ex);
+        return null;
+    }
 }
 
 
-export async function fromPoaNetwork(): Promise<RecommendedGasPrices> { 
+export async function fromPoaNetwork(): Promise<RecommendedGasPrices | null> { 
     
-    const response = await fetch(`https://gasprice.poa.network/`);
-    const body = await response.json();
+    try {
+        const response = await fetch(`https://gasprice.poa.network/`);
+        const body = await response.json();
 
-    return {
-        name: "POA Network",
-        source: "https://gasprice.poa.network/",
-        fast: Math.round(body.fast),
-        average: Math.round(body.standard),
-        low: Math.round(body.slow),
-        lastBlock: Number(body.block_number)
-    } as RecommendedGasPrices;
+        return {
+            name: "POA Network",
+            source: "https://gasprice.poa.network/",
+            fast: Math.round(body.fast),
+            average: Math.round(body.standard),
+            low: Math.round(body.slow),
+            lastBlock: Number(body.block_number)
+        } as RecommendedGasPrices;
+
+    } catch (ex) { 
+        console.log("Couldn't retrieve data from POA NETWORK", ex);
+        return null;
+    }
 }
 
+export async function fromUpvest(): Promise<RecommendedGasPrices | null> { 
 
-export async function fromUpvest(): Promise<RecommendedGasPrices> { 
+    try { 
+        const response = await fetch(`https://fees.upvest.co/estimate_eth_fees`);
+        const body = await response.json();
 
-    const response = await fetch(`https://fees.upvest.co/estimate_eth_fees`);
-    const body = await response.json();
+        return {
+            name: "Upvest",
+            source: "https://doc.upvest.co/reference#ethereum-fees",
+            fast: Math.round(body.estimates.fastest),
+            average: Math.round(body.estimates.medium),
+            low: Math.round(body.estimates.slow),
+            lastUpdate: Date.now()
+        } as RecommendedGasPrices;
 
-    return {
-        name: "Upvest",
-        source: "https://doc.upvest.co/reference#ethereum-fees",
-        fast: Math.round(body.estimates.fastest),
-        average: Math.round(body.estimates.medium),
-        low: Math.round(body.estimates.slow),
-        lastUpdate: Date.now()
-    } as RecommendedGasPrices;
+    } catch (ex) { 
+        console.log("Couldn't retrieve data from UPVEST", ex);
+        return null;
+    }
 }
 
 export function Average(prices: RecommendedGasPrices[], median: boolean) : RecommendedGasPrices { 
