ed4fc31e1cd4e5db0ed0d865bd5d3f914ed0d8a8
wslyvh <wslyvh@users.noreply.github.com>
2020-09-16 10:20:18 +0200

    selector

--- web/src/pages/stats.tsx
+++ web/src/pages/stats.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { AlertStats, GasChart } from '../components';
+import { GasChart } from '../components';
 
 function Stats() {
   return (
     <div className="container">
-      {/* <AlertStats /> */}
       <GasChart />
     </div>
   );
