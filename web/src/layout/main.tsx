a77c00d4184c7511c37a01622853f73026dc1f3d
wslyvh <wslyvh@users.noreply.github.com>
2020-11-26 20:34:46 +0100

    warning

--- web/src/layout/main.tsx
+++ web/src/layout/main.tsx
@@ -14,7 +14,7 @@ function Main() {
 
             <div className="container">
                 <div className="alert alert-warning m-3" role="alert">
-                    ⚠️ Nov 26 - A database issue caused that users did not get flagged when an email was sent. This resulted in multiple notifications without any price change. Apologies for the inconvenience! Trying to fix the issue asap!
+                    <span role="img">⚠️</span> Nov 26 - A database issue caused that users did not get flagged when an email was sent. This resulted in multiple notifications without any price change. Apologies for the inconvenience! Trying to fix the issue asap!
                 </div>
                 {/* <div className="alert alert-warning m-3" role="alert">
                     Enjoy ETH Gas.watch? Support us on <a href="https://gitcoin.co/grants/1175/eth-gaswatch" className="alert-link">Gitcoin Grants</a> round 7. Even a small donation can go a long way 💰
