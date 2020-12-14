4796f369b966f36c8664b276e4d74b1f5c813b37
wslyvh <wslyvh@users.noreply.github.com>
2020-12-13 20:28:17 -0400

    remove tmp cache

--- web/src/functions/gas.ts
+++ web/src/functions/gas.ts
@@ -2,19 +2,16 @@ import { Context, APIGatewayEvent } from 'aws-lambda'
 import { Connect, GetLatestGasData } from '../services/GasService';
 import { GasPriceData } from '../types';
 
-let gasData: GasPriceData;
 Connect().then(() => console.log("GasService Connected"));
 
 export async function handler(event: APIGatewayEvent, context: Context) {
   context.callbackWaitsForEmptyEventLoop = false;
 
-  if (!gasData) {
-    gasData = await GetLatestGasData();
-  }
+  const data = await GetLatestGasData();
 
   return {
       statusCode: 200,
-      body: JSON.stringify(gasData),
+      body: JSON.stringify(data),
       headers: {
         'Cache-Control': 'public, max-age=300',
       },
