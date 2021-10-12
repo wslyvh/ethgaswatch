e34283d5825a02a054052a4fdce1aed3624b8302
wslyvh <wslyvh@users.noreply.github.com>
2021-10-12 10:18:02 +0200

    remove etherchain + gasnow

--- web/src/collectors/GasCollector.ts
+++ web/src/collectors/GasCollector.ts
@@ -1,7 +1,5 @@
 import { RecommendedGasPrices } from "../types";
-import { EtherchainCollector } from "./EtherchainCollector";
 import { EtherscanCollector } from "./EtherscanCollector";
-import { GasNowCollector } from "./GasNowCollector";
 import { GasStationCollector } from "./GasStationCollector";
 import { MyCryptoCollector } from "./MyCryptoCollector";
 import { NetworkCollector } from "./NetworkCollector";
@@ -13,7 +11,8 @@ export class GasCollector {
 
     async Get() : Promise<RecommendedGasPrices[]>{
 
-        const collectors : (typeof Collector)[] = [EtherscanCollector, EtherchainCollector, GasNowCollector, GasStationCollector, MyCryptoCollector, NetworkCollector, UpvestCollector];
+        // remove EtherchainCollector, GasNowCollector
+        const collectors : (typeof Collector)[] = [EtherscanCollector, GasStationCollector, MyCryptoCollector, NetworkCollector, UpvestCollector];
         const results = new Array<RecommendedGasPrices>();
         const gases = await Promise.all(collectors
             .map(async collector => {
